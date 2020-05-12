4a10e45f1e6ab42165741865c4a7512feafb399b
AdonayMorales29 <zadonay111@gmail.com>
2020-05-12 01:08:05 -0600

    avance

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
+import {config} from "dotenv";
+import {resolve} from "path";
 
+config({path:resolve(__dirname,"../../.env")});
 
-
-app.listen(3000,()=>{
-    console.log("Server web prendido en puerto 3000");
+app.listen(3001, ()=>{
+    console.log("Server web prenido en puerto 3000");
 });
 
